4d131df02a576333cd62716340ed02837eec4ce4
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2018-10-01 10:19:33 -0600

    Update testpy.py

--- testpy.py
+++ testpy.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python
 import time
-
-#x = input();
 x = ''
 y = ''.join(sorted(x))
+y = y.replace(' ', '')
 f1 = open ('test.txt','w');
 f1.write('');
 f1.close();
 f = open('test.txt','a')
 z = 0;
 print('Script by NoBannerFlags');
-print('sorting \"'+x+'\" to test.txt');
-print('sorted text is '+y);
+
 for i in y:
 	z = z+1
-	#time.sleep(0.1);
+	
 	if z>=10:
 		f.write('\n');
 		z=0;
 	f.write(i)
-	#print(i);
 f.close();
