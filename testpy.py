894595337e1025a1b20cbee4a0ccadb57caf1ffd
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2018-10-01 10:47:15 -0600

    Update testpy.py

--- testpy.py
+++ testpy.py
@@ -2,7 +2,7 @@
 import time
 def sortMe ( stringtoSort, fileName ):
 	"This sorts the string that is given, filename meaning the file it is stored in."
-	y = ''.join(sorted(x)).lower()
+	y = ''.join(sorted(x.lower()))
 	storeMe = open(fileName,'w')
 	storeMe.write('');
 	storeMe.close();
